9f8a40b1ad3d894196df4aa03b014ba3f60ed412
RitikJain00 <65932447+RitikJain00@users.noreply.github.com>
2025-04-27 19:30:00 +0530

    Delete Backend_Graph directory
    deleted this directory because of some issues

--- Backend_Graph/config.py
+++
@@ -1,9 +0,0 @@
-# config.py
-
-NEO4J_URL = "neo4j+s://351693bf.databases.neo4j.io"
-NEO4J_USER = "neo4j"
-NEO4J_PASSWORD = "btb1HhnEYqIle5KbdkECCfDfFjkH_HvtJj-Av1ekI00"
-NEO4J_DATABASE = "neo4j"
-
-GROQ_API_KEY = "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"
-GROQ_MODEL = "llama3-70b-8192"
